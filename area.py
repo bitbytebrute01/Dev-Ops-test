ae0eecaeda90d4726aad9bf1d5e18d2dd4e69e11
bitbytebrute01 <bitbytebrute01@gmail.com>
2026-01-17 08:36:12 +0530

    initial commit

--- area.py
+++ area.py
@@ -1,11 +1,10 @@
 import math
 import sys
 
-if len(sys.argv[radius] == 1) :
-    radius = sys.argv[1]
+if len(sys.argv) == 2:
+    radius = float(sys.argv[1])
 else:
     radius = 5
 
 area = math.pi * radius * radius
-
 print(area)
